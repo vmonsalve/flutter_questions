57ef1f0ea587d99d8cc8f13580e4b0e8aa4aa9e5
Vicente Monsalve <vmonsalve@vmonsalve.local>
2023-08-15 20:30:24 -0400

    cambios

---
+++ lib/start_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/widgets.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return const Center(
+      child: Text('Start screen'),
+    );
+  }
+}
