ff0a663a81e12808b9f0844671ad7a84f37ceb20
Vicente  Monsalve (ACChi) <141170551+vm-acchi@users.noreply.github.com>
2023-08-26 22:07:25 -0400

    termino pagina inicio app encuesta

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,12 +1,40 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
   @override
   Widget build(context) {
-    return const Center(
-      child: Text('Start screen'),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          //-------------------- Imagen
+          Opacity(
+            opacity: 0.5,
+            child: Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 300,
+            ),
+          ),
+          //-------------------- Texto
+          const SizedBox(height: 80),
+          const Text(
+            'Aprender flutter es divertido!!',
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          const SizedBox(height: 30),
+          // ----------------- Boton
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Ir a las preguntas'),
+          )
+        ],
+      ),
     );
   }
 }
